e42d3eb367c48d22f92168b1d623569cb0e3e1db
Pavan Kumar <pavancse17@gmail.com>
2020-03-31 17:20:12 +0530

    Add twitter streams route

--- api/controllers/TweetsController.js
+++ api/controllers/TweetsController.js
@@ -1,3 +1,5 @@
+const Twitter = require("twitter");
+
 /**
  * TweetsController
  *
@@ -5,8 +7,38 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
-module.exports = {
-  
+let twitter = new Twitter({
+  consumer_key: sails.config.custom.TWITTER_API_KEY,
+  consumer_secret: sails.config.custom.TWITTER_API_SECRET_KEY,
+  access_token_key: sails.config.custom.TWITTER_ACCESS_TOKEN,
+  access_token_secret: sails.config.custom.TWITTER_ACCESS_TOKEN_SECRET
+});
+let twitterStream;
 
+const stream = (socket, searchTerm) => {
+  if (searchTerm) {
+    twitter.stream("statuses/filter", { track: searchTerm }, stream => {
+      stream.on("data", tweet => {
+        socket.emit("tweets", tweet);
+      });
+
+      stream.on("error", error => {
+        console.log("Twitter streaming end point error:", error);
+      });
+      twitterStream = stream;
+    });
+  }
 };
 
+module.exports = {
+  searchTweets: async function(req, res) {
+    let socket = req.socket;
+    if (twitterStream) {
+      twitterStream.destroy();
+    }
+    stream(socket, req.query["q"]);
+    socket.on("connection", () => console.log("Client connected"));
+    socket.on("disconnect", () => console.log("Client disconnected"));
+    return res.ok({ message: "tweets are successfully streaming at tweets" });
+  }
+};
--- config/custom.js
+++ config/custom.js
@@ -9,15 +9,17 @@
  */
 
 module.exports.custom = {
-
   /***************************************************************************
-  *                                                                          *
-  * Any other custom config this Sails app should use during development.    *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Any other custom config this Sails app should use during development.    *
+   *                                                                          *
+   ***************************************************************************/
   // mailgunDomain: 'transactional-mail.example.com',
   // mailgunSecret: 'key-testkeyb183848139913858e8abd9a3',
   // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
   // …
-
+  TWITTER_API_KEY: process.env.TWITTER_API_KEY,
+  TWITTER_API_SECRET_KEY: process.env.TWITTER_API_SECRET_KEY,
+  TWITTER_ACCESS_TOKEN: process.env.TWITTER_ACCESS_TOKEN,
+  TWITTER_ACCESS_TOKEN_SECRET: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
