cdb9ab49847ee978b3dc3d01a23a14e03c407872
Pavan Kumar <pavancse17@gmail.com>
2020-04-01 18:24:12 +0530

    Fix trailing slash

--- config/env/production.js
+++ config/env/production.js
@@ -135,7 +135,7 @@ module.exports = {
     cors: {
       allowOrigins: [
         "https://strawberry-pie-84465.herokuapp.com",
-        "https://pumpkin-tart-12903.herokuapp.com/"
+        "https://pumpkin-tart-12903.herokuapp.com"
       ]
     }
   },
@@ -228,7 +228,7 @@ module.exports = {
      ***************************************************************************/
     onlyAllowOrigins: [
       "https://strawberry-pie-84465.herokuapp.com",
-      "https://pumpkin-tart-12903.herokuapp.com/"
+      "https://pumpkin-tart-12903.herokuapp.com"
     ]
     /***************************************************************************
      *                                                                          *
